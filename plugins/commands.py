c13af382c680cbfce0e41af579ea8e288fca0fbc
I'M OWDVER <56756946+owdver@users.noreply.github.com>
2023-05-07 20:15:40 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -12,8 +12,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
-else:
-    from config import Config
+
 
 from script import Script
 from plugins.helpers import humanbytes
