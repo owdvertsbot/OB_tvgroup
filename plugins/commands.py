c8625455308c090d04f49debbb2fcf5c90a787a7
owdvertsbot <84202766+owdvertsbot@users.noreply.github.com>
2023-05-09 00:33:15 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -133,7 +133,7 @@ async def start(client, message):
                 ]
             ]
         ),
-        reply_to_message_id=message.message.id
+        reply_to_message_id=message.id
     )
     if Config.SAVE_USER == "yes":
         try:
@@ -164,7 +164,7 @@ async def help(client, message):
                 ]
             ]
         ),
-        reply_to_message_id=message.message.id
+        reply_to_message_id=message.id
     )
 
 
@@ -185,5 +185,5 @@ async def about(client, message):
                 ]                
             ]
         ),
-        reply_to_message_id=message.message.id
+        reply_to_message_id=message.id
     )
