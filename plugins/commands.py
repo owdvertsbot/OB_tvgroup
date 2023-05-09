d23688cde0050a69f36dcde2315b382787807fdd
owdvertsbot <84202766+owdvertsbot@users.noreply.github.com>
2023-05-09 13:57:17 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -18,93 +18,124 @@ from database.filters_mdb import filter_stats
 from database.users_mdb import add_user, find_user, all_users
 
 
-@trojanz.on_message(filters.command('id') & (filters.private | filters.group))
+@Client.on_message(filters.command('id'))
 async def showid(client, message):
     chat_type = message.chat.type
-
-    if chat_type == "private":
+    if chat_type == enums.ChatType.PRIVATE:
         user_id = message.chat.id
+        first = message.from_user.first_name
+        last = message.from_user.last_name or ""
+        username = message.from_user.username
+        dc_id = message.from_user.dc_id or ""
         await message.reply_text(
-            f"Your ID : `{user_id}`",
-            parse_mode="md",
+            f"<b>➲ First Name:</b> {first}\n<b>➲ Last Name:</b> {last}\n<b>➲ Username:</b> {username}\n<b>➲ Telegram ID:</b> <code>{user_id}</code>\n<b>➲ Data Centre:</b> <code>{dc_id}</code>",
             quote=True
         )
-    elif (chat_type == "group") or (chat_type == "supergroup"):
-        user_id = message.from_user.id
-        chat_id = message.chat.id
+
+    elif chat_type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
+        _id = ""
+        _id += (
+            "<b>➲ Chat ID</b>: "
+            f"<code>{message.chat.id}</code>\n"
+        )
         if message.reply_to_message:
-            reply_id = f"Replied User ID : `{message.reply_to_message.from_user.id}`"
+            _id += (
+                "<b>➲ User ID</b>: "
+                f"<code>{message.from_user.id if message.from_user else 'Anonymous'}</code>\n"
+                "<b>➲ Replied User ID</b>: "
+                f"<code>{message.reply_to_message.from_user.id if message.reply_to_message.from_user else 'Anonymous'}</code>\n"
+            )
+            file_info = get_file_id(message.reply_to_message)
         else:
-            reply_id = ""
+            _id += (
+                "<b>➲ User ID</b>: "
+                f"<code>{message.from_user.id if message.from_user else 'Anonymous'}</code>\n"
+            )
+            file_info = get_file_id(message)
+        if file_info:
+            _id += (
+                f"<b>{file_info.message_type}</b>: "
+                f"<code>{file_info.file_id}</code>\n"
+            )
         await message.reply_text(
-            f"Your ID : `{user_id}`\nThis Group ID : `{chat_id}`\n\n{reply_id}",
-            parse_mode="md",
+            _id,
             quote=True
-        )   
+        )
 
 
-@trojanz.on_message(filters.command('info') & (filters.private | filters.group))
-async def showinfo(client, message):
-    try:
-        cmd, id = message.text.split(" ", 1)
-    except:
-        id = False
-        pass
 
-    if id:
-        if (len(id) == 10 or len(id) == 9):
-            try:
-                checkid = int(id)
-            except:
-                await message.reply_text("__Enter a valid USER ID__", quote=True, parse_mode="md")
-                return
-        else:
-            await message.reply_text("__Enter a valid USER ID__", quote=True, parse_mode="md")
-            return           
-
-        if Config.SAVE_USER == "yes":
-            name, username, dcid = await find_user(str(id))
-        else:
-            try:
-                user = await client.get_users(int(id))
-                name = str(user.first_name + (user.last_name or ""))
-                username = user.username
-                dcid = user.dc_id
-            except:
-                name = False
-                pass
-
-        if not name:
-            await message.reply_text("__USER Details not found!!__", quote=True, parse_mode="md")
-            return
-    else:
-        if message.reply_to_message:
-            name = str(message.reply_to_message.from_user.first_name\
-                    + (message.reply_to_message.from_user.last_name or ""))
-            id = message.reply_to_message.from_user.id
-            username = message.reply_to_message.from_user.username
-            dcid = message.reply_to_message.from_user.dc_id
-        else:
-            name = str(message.from_user.first_name\
-                    + (message.from_user.last_name or ""))
-            id = message.from_user.id
-            username = message.from_user.username
-            dcid = message.from_user.dc_id
-    
-    if not str(username) == "None":
-        user_name = f"@{username}"
-    else:
-        user_name = "none"
-
-    await message.reply_text(
-        f"<b>Name</b> : {name}\n\n"
-        f"<b>User ID</b> : <code>{id}</code>\n\n"
-        f"<b>Username</b> : {user_name}\n\n"
-        f"<b>Permanant USER link</b> : <a href='tg://user?id={id}'>Click here!</a>\n\n"
-        f"<b>DC ID</b> : {dcid}\n\n",
-        quote=True,
-        parse_mode="html"
+@Client.on_message(filters.command(["info"]))
+async def who_is(client, message):
+    # https://github.com/SpEcHiDe/PyroGramBot/blob/master/pyrobot/plugins/admemes/whois.py#L19
+    status_message = await message.reply_text(
+        "`Fetching user info...`"
     )
+    await status_message.edit(
+        "`Processing user info...`"
+    )
+    from_user = None
+    from_user_id, _ = extract_user(message)
+    try:
+        from_user = await client.get_users(from_user_id)
+    except Exception as error:
+        await status_message.edit(str(error))
+        return
+    if from_user is None:
+        return await status_message.edit("no valid user_id / message specified")
+    message_out_str = ""
+    message_out_str += f"<b>➲First Name:</b> {from_user.first_name}\n"
+    last_name = from_user.last_name or "<b>None</b>"
+    message_out_str += f"<b>➲Last Name:</b> {last_name}\n"
+    message_out_str += f"<b>➲Telegram ID:</b> <code>{from_user.id}</code>\n"
+    username = from_user.username or "<b>None</b>"
+    dc_id = from_user.dc_id or "[User Doesn't Have A Valid DP]"
+    message_out_str += f"<b>➲Data Centre:</b> <code>{dc_id}</code>\n"
+    message_out_str += f"<b>➲User Name:</b> @{username}\n"
+    message_out_str += f"<b>➲User 𝖫𝗂𝗇𝗄:</b> <a href='tg://user?id={from_user.id}'><b>Click Here</b></a>\n"
+    if message.chat.type in ((enums.ChatType.SUPERGROUP, enums.ChatType.CHANNEL)):
+        try:
+            chat_member_p = await message.chat.get_member(from_user.id)
+            joined_date = (
+                chat_member_p.joined_date or datetime.now()
+            ).strftime("%Y.%m.%d %H:%M:%S")
+            message_out_str += (
+                "<b>➲Joined this Chat on:</b> <code>"
+                f"{joined_date}"
+                "</code>\n"
+            )
+        except UserNotParticipant:
+            pass
+    chat_photo = from_user.photo
+    if chat_photo:
+        local_user_photo = await client.download_media(
+            message=chat_photo.big_file_id
+        )
+        buttons = [[
+            InlineKeyboardButton('ᴄʟᴏsᴇ', callback_data='close_data')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await message.reply_photo(
+            photo=local_user_photo,
+            quote=True,
+            reply_markup=reply_markup,
+            caption=message_out_str,
+            parse_mode=enums.ParseMode.HTML,
+            disable_notification=True
+        )
+        os.remove(local_user_photo)
+    else:
+        buttons = [[
+            InlineKeyboardButton('ᴄʟᴏsᴇ', callback_data='close_data')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await message.reply_text(
+            text=message_out_str,
+            reply_markup=reply_markup,
+            quote=True,
+            parse_mode=enums.ParseMode.HTML,
+            disable_notification=True
+        )
+    await status_message.delete()
 
 
 @trojanz.on_message((filters.private | filters.group) & filters.command('status'))
