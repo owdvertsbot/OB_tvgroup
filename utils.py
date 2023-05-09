b57d530c67a0752141e237cd2a990da78b73dbc8
owdvertsbot <84202766+owdvertsbot@users.noreply.github.com>
2023-05-09 15:58:31 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -40,7 +40,7 @@ class temp(object):
 
 
 
-async def extract_user(message: Message) -> Union[int, str]:
+def extract_user(message: Message) -> Union[int, str]:
     """extracts the user from a message"""
     # https://github.com/SpEcHiDe/PyroGramBot/blob/f30e2cca12002121bad1982f68cd0ff9814ce027/pyrobot/helper_functions/extract_user.py#L7
     user_id = None
