6cb71fccc267fe1000fdf1f1b6a8c3bd6668545e
owdvertsbot <84202766+owdvertsbot@users.noreply.github.com>
2023-05-12 16:40:55 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -11,7 +11,6 @@ import urllib.parse
 import tvdb_api
 
 from datetime import datetime
-from pyrogram import filters
 from pyrogram import Client, filters, enums
 from pyrogram.errors import ChatAdminRequired, FloodWait, BadRequest
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
@@ -22,7 +21,6 @@ from info import SAVE_USER, PICS, THETVDB_API_KEY
 from plugins.helpers import humanbytes
 from database.filters_mdb import filter_stats
 from database.users_mdb import add_user, find_user, all_users
-from dotenv import load_dotenv
 
 
 @Client.on_message(filters.command('id'))
@@ -107,63 +105,6 @@ async def start(client, message):
         except:
             pass
         
-t = Tvdb(apikey="fe9c05b0-2099-4c03-b0dd-91ee77dfa192") # doctest:+SKIP   
         
         
-# Function to retrieve TV show information and landscape poster
-def get_tvshow_info(name):
-    try:
-        url = f'https://api.thetvdb.com/search/series?name={name}'
-        headers = {'Content-Type': 'application/json'}
-        data = {'apikey': 'fe9c05b0-2099-4c03-b0dd-91ee77dfa192'}
-        response = requests.post(url, headers=headers, data=json.dumps(data)).json()
-        if response['data']:
-            tv_show = response['data'][0]
-            title = tv_show['seriesName']
-            overview = tv_show['overview']
-            poster_path = tv_show['banner']
-            if poster_path:
-                poster_url = f'https://www.thetvdb.com/banners/{poster_path}'
-                return title, overview, poster_url
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-    except KeyError as e:
-        print(f"Error: {e}")
-    except Exception as e:
-        print(f"Error: {e}")
-    return None
-
-# Function to check if message is a TV show name
-def is_tvshow(message_text):
-    print(f"Checking if '{message_text}' is a TV show name")
-    # Use regular expression to match TV show names
-    regex = r'\b([Tt][Vv]\s*[Ss]\d{2}([Ee]?\d{2})*)|([Tt][Vv]\s*[Ss]\d{1,2}\s*[Ee]\d{1,2})|([Tt][Vv]\s*series)\b'
-    return re.search(regex, message_text)
-
-# on_message function to handle incoming messages
-@Client.on_message(filters.text & filters.group & filters.bot)
-async def on_message(client, message):
-    # Check if message is a TV show name
-    if is_tvshow(message.text):
-        # Get TV show information and landscape poster
-        tvshow_info = get_tvshow_info(message.text)
-        print(f"TV Show Info: {tvshow_info}")
-        if tvshow_info:
-            title, overview, poster_url = tvshow_info
-            # Send TV show information and landscape poster
-            try:
-                await message.reply_text(f'{title}\n\n{overview}')
-                await message.reply_photo(photo=poster_url)
-            except pyrogram.errors.exceptions.bad_request_400.PhotoInvalidDimensions:
-                await message.reply_text(f"{title}\n\n{overview}\n\nSorry, I could not send the poster because it's dimensions are invalid.")
-            except pyrogram.errors.exceptions.bad_request_400.PhotoContentUrlEmpty:
-                await message.reply_text(f"{title}\n\n{overview}\n\nSorry, I could not send the poster because the URL is empty.")
-            except pyrogram.errors.exceptions.bad_request_400.PhotoIdInvalid:
-                await message.reply_text(f"{title}\n\n{overview}\n\nSorry, I could not send the poster because the photo ID is invalid.")
-            except pyrogram.errors.exceptions.bad_request_400.PhotoInvalid:
-                await message.reply_text(f"{title}\n\n{overview}\n\nSorry, I could not send the poster because the photo is invalid.")
-            except pyrogram.errors.exceptions.bad_request_400.PhotoContentUrlInvalid:
-                await message.reply_text(f"{title}\n\n{overview}\n\nSorry, I could not send the poster because the URL is invalid.")
-            except pyrogram.errors.exceptions.bad_request_400.PhotoRemoteFileInvalid:
-                await message.reply_text('Error: Invalid photo file')
 
