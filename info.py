602f37338dac985d271325ec698071d37507ac66
owdvertsbot <84202766+owdvertsbot@users.noreply.github.com>
2023-05-12 20:46:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -34,4 +34,3 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
