91232aad4a3846c02943f2902e70a526ea437a45
owdvertsbot <84202766+owdvertsbot@users.noreply.github.com>
2023-05-12 15:55:13 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -144,7 +144,7 @@ async def on_message(client, message):
     if is_tvshow(message.text):
         # Get TV show information and landscape poster
         tvshow_info = get_tvshow_info(message.text)
-        print(tvshow_info)
+        print(f"tvshow_info: {tvshow_info}")  # Print the output of get_tvshow_info
         if tvshow_info:
             title, overview, poster_url = tvshow_info
             # Send TV show information and landscape poster
