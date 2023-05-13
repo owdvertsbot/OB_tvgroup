099d02e47f51cc1c2c2d5f7a7a21d3a65592e819
owdvertsbot <84202766+owdvertsbot@users.noreply.github.com>
2023-05-13 12:57:51 +0530

    Update filters_mdb.py

--- database/filters_mdb.py
+++ database/filters_mdb.py
@@ -1,12 +1,14 @@
+import os
+import re
 import pymongo
-from pyrogram import enums
-from info import DATABASE_URI, DATABASE_NAME
-import logging
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.ERROR)
 
-myclient = pymongo.MongoClient(DATABASE_URI)
-mydb = myclient[DATABASE_NAME]
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+ 
+myclient = pymongo.MongoClient(Config.DATABASE_URI)
+mydb = myclient[Config.DATABASE_NAME]
 
 
 
