7369ad5f49c69aa72d97ceadea6c2dcba2c83777
owdvertsbot <84202766+owdvertsbot@users.noreply.github.com>
2023-06-02 18:29:03 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -101,3 +101,24 @@ async def start(client, message):
             )
         except:
             pass
+        
+# Set up the TMDB API client
+tmdb = TMDb()
+tmdb.api_key = os.environ.get("9555335f868ed5bce03a57c35fa9da19")
+tv = TV()
+
+@Client.on_message(filters.command('tvshow')
+def tv_show_info(client, message):
+    show_name = message.text
+    search_results = tv.search(show_name)
+    if len(search_results) == 0:
+        response = "Sorry, I couldn't find any information about that TV show."
+    else:
+        tv_show = search_results[0]
+        response = f"Title: {tv_show.name}\n"
+        response += f"Overview: {tv_show.overview}\n"
+        response += f"First Air Date: {tv_show.first_air_date}\n"
+        response += f"Vote Average: {tv_show.vote_average}\n"
+
+    client.send_message(chat_id=message.chat.id, text=response)
+
