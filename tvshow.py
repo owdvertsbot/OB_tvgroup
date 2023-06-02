263b6886bb6238befc3d17253ffd728ad6f9ce4e
owdvertsbot <84202766+owdvertsbot@users.noreply.github.com>
2023-06-02 17:53:18 +0530

    Update tvshow.py

--- tvshow.py
+++ tvshow.py
@@ -1,41 +1,52 @@
-import requests
+import os
+import urllib
+from pyrogram import Client
+from tmdbv3api import TMDb, TV
 
-# Replace YOUR_API_KEY with your actual API key
-API_KEY = '9555335f868ed5bce03a57c35fa9da19'
 
-# Set up the API endpoint URL
-url = f'https://api.themoviedb.org/3/tv/1399?api_key={API_KEY}'
+tmdb = TMDb()
+tmdb.api_key = "9555335f868ed5bce03a57c35fa9da19"
 
-# Make the API request
-response = requests.get(url)
+tv = TV()
 
-# Check if the request was successful
-if response.status_code == 200:
-    # Parse the JSON response
-    data = response.json()
 
-    # Extract the relevant information
-    poster_path = data['poster_path']
-    title = data['name']
-    year = data['first_air_date'][:4]
-    seasons = data['number_of_seasons']
-    total_episodes = data['number_of_episodes']
-    run_time = data['episode_run_time'][0]
-    actors = [actor['name'] for actor in data['credits']['cast'][:5]]
-    directors = [crew['name'] for crew in data['credits']['crew'] if crew['job'] == 'Director']
-    genres = [genre['name'] for genre in data['genres']]
-    streaming_services = ['HBO', 'HBO Max']
+def send_tv_show_details(bot, chat_id, tv_show_name):
+    # Search for the TV show
+    results = tv.search(tv_show_name)
+    if not results:
+        bot.send_message(chat_id, "TV show not found.")
+        return
 
-    # Print the TV show information
-    print(f'TV Show Poster: ![alt text](https://image.tmdb.org/t/p/w500{poster_path})')
-    print(f'Caption: {title} ({year})')
-    print(f'Seasons: {seasons}')
-    print(f'Total Episodes: {total_episodes}')
-    print(f'Run Time: {run_time} minutes per episode')
-    print(f'Actors: {actors}')
-    print(f'Directors: {directors}')
-    print(f'Genre: {genres}')
-    print(f'Where to Watch: {streaming_services}')
-else:
-    # Print an error message if the request failed
-    print(f'Request failed with status code {response.status_code}')
+    tv_show = results[0]
+    tv_show_id = tv_show.id
+
+    # Get the details of the TV show
+    tv_show_details = tv.get_tv_show_details(tv_show_id)
+
+    # Download the poster image
+    poster_path = tv_show_details.poster_path
+    if poster_path:
+        poster_url = tmdb.image_url + "w500" + poster_path
+        poster_file_name = f"{tv_show_id}_poster.jpg"
+        urllib.request.urlretrieve(poster_url, poster_file_name)
+
+        # Send the TV show details and poster
+        bot.send_photo(chat_id, photo=poster_file_name, caption=tv_show_details.overview)
+    else:
+        bot.send_message(chat_id, "Poster not available for this TV show.")
+    
+    # Clean up the downloaded poster file
+    os.remove(poster_file_name)
+
+    
+def main():
+    with app:
+        @app.on_message()
+        def handle_message(client, message):
+            if message.text:
+                # Extract the TV show name from the user's query
+                tv_show_name = message.text
+
+                # Send the TV show details to the user
+                send_tv_show_details(client, message.chat.id, tv_show_name)
+    
